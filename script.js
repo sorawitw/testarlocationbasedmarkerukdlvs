933b1de4d95ad2ba71f4fc6b354da2b48b6da5f1
sorawitw <rispro.ray@gmail.com>
2022-01-21 18:13:59 +0700

    add lat and log for testing

--- script.js
+++ script.js
@@ -13,8 +13,8 @@ function staticLoadPlaces() {
             name: 'Pokèmon',
             location: {
                 // decomment the following and add coordinates:
-                // lat: <your-latitude>,
-                // lng: <your-longitude>,
+                lat: 13.687970700098788,
+                lng: 100.29605631954689,
             },
         },
     ];
